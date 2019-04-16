e96a742a0fede7334e059a3a786195eaf91de19c
kill4n <kill4n@users.noreply.github.com>
2019-04-16 10:10:48 -0500

    Update ipeam.py

--- ipeam.py
+++ ipeam.py
@@ -1,25 +1,25 @@
-import pandas as pd
-import numpy as np
-from plotnine import *
-import urllib.request
-import csv
-import geoip2.database
-
-db = geoip2.database.Reader('./GeoLite2-City.mmdb')
-
-with open('Descargas_May2018.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count =0
-    for row in csv_reader:
-        try:
-            response = db.city(row[5])
-            print (row[5], response.country.name, response.city.name, response.country.iso_code, response.location.longitude,   response.location.latitude)
-        except:
-            pass
-        line_count += 1
-    #print(f'Processed {line_count} lines.')
 
 
+data = {}
 
-#np.savetxt("newDataFile.csv", data, fmt='%.18e', delimiter=', ', header='New Data', comments='# ')
+df = open("Mayo2018Descargas.txt")
+
+#p  cambia las columnas 1 -> pais, 2 -> ciudad
+p=1
+
+for line in df.readlines():
+    sf = line.split()
+    
+    myValue = data.get(sf[p], 0)
+
+    if(myValue == 0):
+        data.update({sf[p]:1})
+    else:
+        data.update({sf[p]:1+myValue})
+
+    #pass        
+    #print(myValue)
+    
+    
+print(data)
 
